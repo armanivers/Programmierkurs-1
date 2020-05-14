b72133e6fbd27b45072d0641be6e08276ba9dd65
Ariii <armanivers@hotmail.com>
2020-05-14 18:03:51 +0200

    exception handling

--- src/mainprogramm/Menu.java
+++ src/mainprogramm/Menu.java
@@ -1,24 +1,27 @@
 package mainprogramm;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class Menu {
 	static Medienverwaltung verwaltung = new Medienverwaltung();
-
+	static boolean init = false;
+	
 	public static void start() {
-
-		// Beispielmedien
-		Audio audio1 = new Audio("It Means Nothing", 2007, "Stereophonics", 229);
-		Audio audio2 = new Audio("It Means Nothing", 2010, "Stereophonics", 229);
-		Bild bild1 = new Bild("Gebaude FB Informatik", 2019, "Dortmund");
-		Bild bild2 = new Bild("Eiffelturm", 2020, "Paris");
-
-		verwaltung.aufnehmen(audio1);
-		verwaltung.aufnehmen(audio2);
-		verwaltung.aufnehmen(bild1);
-		verwaltung.aufnehmen(bild2);
+		if(!init) {
+			// Beispielmedien
+			Audio audio1 = new Audio("It Means Nothing", 2007, "Stereophonics", 229);
+			Audio audio2 = new Audio("It Means Nothing", 2010, "Stereophonics", 229);
+			Bild bild1 = new Bild("Gebaude FB Informatik", 2019, "Dortmund");
+			Bild bild2 = new Bild("Eiffelturm", 2020, "Paris");
+			verwaltung.aufnehmen(audio1);
+			verwaltung.aufnehmen(audio2);
+			verwaltung.aufnehmen(bild1);
+			verwaltung.aufnehmen(bild2);
+			init = true;
+		}
 
 		Scanner s = new Scanner(System.in);
 		int auswahl;
@@ -29,27 +32,32 @@ public class Menu {
 			// Menupunkte auf Konsole anzeigen
 			System.out.printf("1. Audio aufnehmen \n2. Bild aufnehmen \n3. Zeige alle Medien \n4. Zeige neues Medium "
 					+ "\n5. Berechne durschnittliches Erscheinungsjahr \n6. Beenden \n \nBitte Menuepunkt waehlen:");
-			auswahl = s.nextInt();
-			switch (auswahl) {
-			case 1:
-				addAudio();
-				break;
-			case 2:
-				addBild();
-				break;
-			case 3:
-				verwaltung.zeigeMedien();
-				break;
-			case 4:
-				verwaltung.sucheNeuesMedium();
-				break;
-			case 5:
-				System.out.println(verwaltung.berechneErscheinungsjahr());
-				break;
-			case 6:
-				run = false;
-				break;
-			default:
+			try {
+				auswahl = s.nextInt();
+				switch (auswahl) {
+				case 1:
+					addAudio();
+					break;
+				case 2:
+					addBild();
+					break;
+				case 3:
+					verwaltung.zeigeMedien();
+					break;
+				case 4:
+					verwaltung.sucheNeuesMedium();
+					break;
+				case 5:
+					System.out.println(verwaltung.berechneErscheinungsjahr());
+					break;
+				case 6:
+					run = false;
+					break;
+				default:
+				}
+			}catch(InputMismatchException e) {
+				System.out.println("Bitte eine gueltige Zahl eingeben!");
+				start();
 			}
 
 		}
